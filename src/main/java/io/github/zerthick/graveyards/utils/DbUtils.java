e16ec47d44b1c427b8f46ec23a8b5e8fa81e084a
Zerthick <zerthick@gmail.com>
2015-10-14 23:49:46 -0400

    Updated DB to store a separate table per world.

--- src/main/java/io/github/zerthick/graveyards/utils/DbUtils.java
+++ src/main/java/io/github/zerthick/graveyards/utils/DbUtils.java
@@ -21,11 +21,9 @@ package io.github.zerthick.graveyards.utils;
 
 import com.flowpowered.math.vector.Vector3i;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DbUtils {
 
@@ -40,28 +38,27 @@ public class DbUtils {
                 c = DriverManager.getConnection("jdbc:sqlite:Graveyards.db");
                 stmt = c.createStatement();
 
-                // Remove table if it already exists and create a new one
-                String sql = ("DROP TABLE IF EXISTS GRAVEYARDS");
-                stmt.executeUpdate(sql);
-
-                sql = "CREATE TABLE GRAVEYARDS" +
-                        "(NAME TEXT PRIMARY KEY     NOT NULL," +
-                        "WORLDID            TEXT     NOT NULL," +
-                        "X                  INT     NOT NULL," +
-                        "Y                  INT     NOT NULL," +
-                        "Z                  INT     NOT NULL)";
-                stmt.executeUpdate(sql);
+                //Clean DB of old worlds and graveyards
+                cleanDB(c, graveyardMap);
 
                 // Iterate through graveyard map and add each graveyard to the db
                 for (UUID worldUUID : graveyardMap.keySet()) {
-                    Set<Graveyard> graveyardSet = graveyardMap.get(worldUUID);
 
+                    String sql = "CREATE TABLE IF NOT EXISTS '" +
+                            worldUUID.toString() +
+                            "'(NAME TEXT PRIMARY KEY     NOT NULL," +
+                            "X                  INT     NOT NULL," +
+                            "Y                  INT     NOT NULL," +
+                            "Z                  INT     NOT NULL)";
+                    stmt.executeUpdate(sql);
+
+                    Set<Graveyard> graveyardSet = graveyardMap.get(worldUUID);
                     for (Graveyard graveyard : graveyardSet) {
                         String graveyardName = graveyard.getName();
                         Vector3i graveyardLoc = graveyard.getLocation();
 
-                        sql = "INSERT INTO GRAVEYARDS (NAME,WORLDID,X,Y,Z) " +
-                                "VALUES ('" + graveyardName + "','" + worldUUID.toString() + "',"
+                        sql = "INSERT OR REPLACE INTO '" + worldUUID.toString() +"' (NAME,X,Y,Z) " +
+                                "VALUES ('" + graveyardName + "',"
                                 + graveyardLoc.getX() + ',' + graveyardLoc.getY() + ',' + graveyardLoc.getZ() + ");";
                         stmt.executeUpdate(sql);
                     }
@@ -81,6 +78,7 @@ public class DbUtils {
 
         Connection c;
         Statement stmt;
+        DatabaseMetaData md;
 
         try {
             // Create sqlite connection
@@ -89,23 +87,55 @@ public class DbUtils {
             c.setAutoCommit(false);
             stmt = c.createStatement();
 
-            ResultSet rs = stmt.executeQuery("SELECT * FROM GRAVEYARDS;");
+            // Get list of tables in db
+            ResultSet tableNames = stmt.executeQuery("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name");
+            while (tableNames.next()) {
+                String tableName = tableNames.getString("name");
+                System.out.println(tableName);
+                ResultSet graveyardRecords = stmt.executeQuery("SELECT * FROM '" + tableName + "';");
 
-            // Iterate through db and add each graveyard to the map
-            while (rs.next()) {
-                String graveyardName = rs.getString("NAME");
-                UUID worldUUID = UUID.fromString(rs.getString("WORLDID"));
-                Vector3i graveyardLocation = new Vector3i(rs.getInt("X"), rs.getInt("Y"), rs.getInt("Z"));
+                // Iterate through table and add each graveyard to the map
+                while (graveyardRecords.next()) {
+                    String graveyardName = graveyardRecords.getString("NAME");
+                    UUID worldUUID = UUID.fromString(tableName);
+                    Vector3i graveyardLocation = new Vector3i(graveyardRecords.getInt("X"), graveyardRecords.getInt("Y"), graveyardRecords.getInt("Z"));
 
-                Set<Graveyard> graveyardSet = graveyardMap.getOrDefault(worldUUID, new HashSet<>());
-                graveyardSet.add(new Graveyard(graveyardName, graveyardLocation));
-                graveyardMap.put(worldUUID, graveyardSet);
+                    Set<Graveyard> graveyardSet = graveyardMap.getOrDefault(worldUUID, new HashSet<>());
+                    graveyardSet.add(new Graveyard(graveyardName, graveyardLocation));
+                    graveyardMap.put(worldUUID, graveyardSet);
+                }
             }
-
             stmt.close();
             c.close();
         } catch (Exception ignore) {
+            ignore.printStackTrace();
         }
         return graveyardMap;
+    }
+
+    private static void cleanDB(Connection c, Map<UUID, Set<Graveyard>> graveyardMap) throws SQLException {
+
+        Statement stmt = c.createStatement();
+
+        Set<UUID> worldUUIDs = graveyardMap.keySet();
+        ResultSet tableNames = stmt.executeQuery("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name");
+        while(tableNames.next()){
+            String tableName = tableNames.getString("name");
+            UUID worldUUID = UUID.fromString(tableName);
+
+            if(!worldUUIDs.contains(worldUUID)){ //Remove Old Worlds
+                stmt.executeUpdate("DROP TABLE '" + tableName + "'");
+            } else { //Clean Table for old Graveyards
+                Set<String> graveyardNames = graveyardMap.get(worldUUID).stream().map(Graveyard::getName).collect(Collectors.toSet());
+                ResultSet graveyardRecords = stmt.executeQuery("SELECT * FROM '" + tableName + "';");
+                while (graveyardRecords.next()){
+                    String recordName = graveyardRecords.getString("NAME");
+                    if(!graveyardNames.contains(recordName)){
+                        stmt.executeUpdate("DELETE FROM '" + tableName + "' WHERE NAME = '" + recordName + "';");
+                    }
+                }
+            }
+        }
+        stmt.close();
     }
 }
